f27f210ff724896bf9d3d5e936db55f3450a5e81
smhamidi <1380hamidi@gmail.com>
2023-08-15 15:04:38 +0330

    First Project done, now statements

---
+++ 1-Section-Diving-In/1-First-Project/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+
+  std::cout << "Number 1" << std::endl;
+  std::cout << "Number 2" << std::endl;
+
+  for (int i = 0; i < 5; i++) {
+    std::cout << "Seyyed Mohammad Hamidi" << std::endl;
+  }
+
+  /*
+  This is How we write Comments (Multiline)
+  */
+
+  // This is another way to write comment
+
+  // The next line will give you warning if you uncomment
+  // std::cout << 10 / 0 << std::endl;
+  return 0;
+}
--- 1-diving-in/1-first-program/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-// consteval int get_value() { return 3; }
-
-int main() {
-  std::cout << "Hello World!" << std::endl;
-  return 0;
-}
